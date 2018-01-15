fae16f749c53d1c218c1b88c5955c27ed00c5d7c
Paula Kochańska <261681@pw.edu.pl>
2018-01-15 13:01:16 +0100

    Dodaj pliki projektów.

---
+++ ThreadingApp/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingApp
+{
+    class Program
+    {
+        static int GLOBAL_VALUE = 10;
+        static readonly object locker = new object();
+        static bool done;
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+
+            Thread t = new Thread(new ThreadStart(Increment));
+            t.Name = "Uno";
+            Thread t2 = new Thread(Increment);
+            t2.Name = "Dos";
+
+            Thread t3 = new Thread(new ThreadStart(() => Decrement(5)));
+            t3.Name = "Tres";
+            Thread t4 = new Thread(() => Decrement(10));
+            t4.Name = "Quatro";
+
+            t.Start(); 
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            /*t.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();*/
+
+
+            //t.Abort();
+            //t4.Abort();
+
+            System.Console.WriteLine("Main thread at the end");
+            System.Console.WriteLine("Global value at the end: " + GLOBAL_VALUE);
+            System.Console.ReadKey();
+
+        }
+
+        public static void Increment()
+        {            
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    System.Console.WriteLine(i + " " + Thread.CurrentThread.Name);
+                    Thread.Sleep(10);
+
+                    lock (locker)
+                    {
+                        if (!done)
+                        {
+                            int tmp = GLOBAL_VALUE;
+                            tmp += 1;
+                            GLOBAL_VALUE = tmp;
+                            done = true;
+                            System.Console.WriteLine(Thread.CurrentThread.Name + " Global value: " + GLOBAL_VALUE);
+                        }
+                    }
+                    done = false;
+
+                }
+                catch (ThreadAbortException ex)
+                {
+                    System.Console.WriteLine("Thread " + Thread.CurrentThread.Name + " aborted");
+                    Thread.CurrentThread.Abort();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static void Decrement(int max)
+        {
+            for (int i = max; i >= 0; i--)
+            {
+                try
+                { 
+                    System.Console.WriteLine(i + " " + Thread.CurrentThread.Name);
+
+                    lock (locker)
+                    {
+                        if (!done)
+                        {
+                            int tmp = GLOBAL_VALUE;
+                            tmp -= 1;
+                            GLOBAL_VALUE = tmp;
+                            done = true;
+                            System.Console.WriteLine(Thread.CurrentThread.Name + " Global value: " + GLOBAL_VALUE);
+                        }
+                    }
+                    done = false;
+
+                }
+                catch (ThreadAbortException ex)
+                {
+                    System.Console.WriteLine("Thread " + Thread.CurrentThread.Name + " aborted");
+                    Thread.CurrentThread.Abort();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ ThreadingApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ThreadingApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadingApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("ad1b19f0-e2d6-4793-8454-1b7ed8355cc7")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
